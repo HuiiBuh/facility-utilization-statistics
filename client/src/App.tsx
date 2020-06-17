1e6f4b15f538f5182ba7fbdebc4acdc6cae98a1b
HuiiBuh <nhaderer1@gmail.com>
2020-06-17 20:23:37 +0200

    Minor changes to the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+class World extends React.Component {
+    render() {
+        return 'Hello World Two';
+    }
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <p>Hello World</p>
+            <World/>
+        </div>
+    );
 }
 
 export default App;
