3be7d85c9ff25119b378a61d5a5e4fddcf4a9048
HuiiBuh <nhaderer1@gmail.com>
2020-06-26 15:56:40 +0200

    Changes

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,20 @@
-import {Controller, Get, Param, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
-import {AppService} from './app.service';
-import {FileInterceptor} from '@nestjs/platform-express';
-import {DataType} from './storage/Storage';
+import {Controller, ForbiddenException, Get, Param, Post, Req, UploadedFile, UseInterceptors} from "@nestjs/common";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {Request} from "express";
+
+import {AppService} from "./app.service";
+import {DataType} from "./storage/Storage";
+
+const globalAccess = "";
+
+interface File {
+    fieldname: string;
+    originalname: string;
+    encoding: string;
+    mimetype: string;
+    buffer: Buffer;
+    size: number;
+}
 
 @Controller()
 export class AppController {
@@ -14,28 +27,25 @@ export class AppController {
                 <a href="/kletterbox">Kletterbox</a>`;
     }
 
-    @Get('bloeckle')
+    @Get("bloeckle")
     getBloeckle(): DataType {
         return this.appService.getBloeckle();
     }
 
-    @Get('kletterbox')
+    @Get("kletterbox")
     getKletterbox(): DataType {
         return this.appService.getKletterbox();
     }
 
-    @Post('upload/:id')
-    @UseInterceptors(FileInterceptor('file'))
-    uploadFile(@UploadedFile() file: File, @Param('id') id: string): void {
+    @Post("upload/:id")
+    @UseInterceptors(FileInterceptor("file"))
+    uploadFile(@UploadedFile() file: File, @Param("id") id: string, @Req() request: Request): void {
+
+        const accessKey = request.headers["x-access-key"];
+        if (accessKey !== globalAccess) {
+            throw new ForbiddenException("Wrong password");
+        }
+
         this.appService.mergeDatabase(id, file.buffer);
     }
-}
-
-interface File {
-    fieldname: string;
-    originalname: string;
-    encoding: string;
-    mimetype: string;
-    buffer: Buffer;
-    size: number;
 }
